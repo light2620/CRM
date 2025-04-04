095e341bd6375a3eaa8274e5793195e0567e42de
shivam <shivamnegi896@gmail.com>
2025-04-04 21:28:39 +0530

    changed company model

--- Backend/Models/CompnayModel.js
+++ Backend/Models/CompnayModel.js
@@ -4,10 +4,16 @@ import mongoose from "mongoose";
 
 const CompanySchema = new mongoose.Schema({
     name: { type: String, required: true },
-    address: { type: String },
+    address: {
+      addressLine: { type: String },
+      city: { type: String },
+      state: { type: String },
+      pincode: { type: String }
+    },
     contactEmail: { type: String },
     contactPhone: { type: String }
-});
+  });
+  
 
 
 const CompanyModel = mongoose.model("company",CompanySchema);
